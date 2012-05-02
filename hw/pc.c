cf411d92ffd15761c45ed3ff7b7ff7d60871e8e9
Andreas Färber <afaerber@suse.de>
2012-05-02 19:10:59 +0200

    pc: Add CPU as /machine/cpu[n]
    Using the cpu_index, give the X86CPU a canonical path.
    This must be done before initializing the APIC.
    
    Signed-off-by: Igor Mammedov <niallain@gmail.com>
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- hw/pc.c
+++ hw/pc.c
@@ -945,6 +945,8 @@ static X86CPU *pc_new_cpu(const char *cpu_model)
 {
     X86CPU *cpu;
     CPUX86State *env;
+    char *name;
+    Error *error = NULL;
 
     cpu = cpu_x86_init(cpu_model);
     if (cpu == NULL) {
@@ -952,6 +954,16 @@ static X86CPU *pc_new_cpu(const char *cpu_model)
         exit(1);
     }
     env = &cpu->env;
+
+    name = g_strdup_printf("cpu[%d]", env->cpu_index);
+    object_property_add_child(OBJECT(qdev_get_machine()), name,
+                              OBJECT(cpu), &error);
+    g_free(name);
+    if (error_is_set(&error)) {
+        qerror_report_err(error);
+        exit(1);
+    }
+
     if ((env->cpuid_features & CPUID_APIC) || smp_cpus > 1) {
         env->apic_state = apic_init(env, env->cpuid_apic_id);
     }
