2f1c55607e5c462bf22ff2ad4aa050041f31bbfd
Andreas Färber <afaerber@suse.de>
2013-01-17 12:13:41 +0100

    cpu: Add CPUArchState pointer to CPUState
    The target-specific ENV_GET_CPU() macros have allowed us to navigate
    from CPUArchState to CPUState. The reverse direction was not supported.
    Avoid introducing CPU_GET_ENV() macros by initializing an untyped
    pointer that is initialized in derived instance_init functions.
    
    The field may not be called "env" due to it being poisoned.
    
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- target-cris/cpu.c
+++ target-cris/cpu.c
@@ -68,10 +68,12 @@ static void cris_cpu_realizefn(DeviceState *dev, Error **errp)
 
 static void cris_cpu_initfn(Object *obj)
 {
+    CPUState *cs = CPU(obj);
     CRISCPU *cpu = CRIS_CPU(obj);
     CPUCRISState *env = &cpu->env;
     static bool tcg_initialized;
 
+    cs->env_ptr = env;
     cpu_exec_init(env);
 
     if (tcg_enabled() && !tcg_initialized) {
