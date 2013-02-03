29e128a52568ff14f28eda4a412d173b44c8fd5c
Andreas Färber <afaerber@suse.de>
2013-02-03 22:00:02 +0100

    target-ppc: Extract 7450 alias
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- target-ppc/translate_init.c
+++ target-ppc/translate_init.c
@@ -7353,7 +7353,6 @@ enum {
     CPU_POWERPC_7448_v11           = 0x80040101,
     CPU_POWERPC_7448_v20           = 0x80040200,
     CPU_POWERPC_7448_v21           = 0x80040201,
-#define CPU_POWERPC_7450             CPU_POWERPC_7450_v21
     CPU_POWERPC_7450_v10           = 0x80000100,
     CPU_POWERPC_7450_v11           = 0x80000101,
     CPU_POWERPC_7450_v12           = 0x80000102,
@@ -8877,8 +8876,6 @@ static const ppc_def_t ppc_defs[] = {
     POWERPC_DEF("7448_v2.0",     CPU_POWERPC_7448_v20,               7400)
     /* PowerPC 7448 v2.1 (G4)                                                */
     POWERPC_DEF("7448_v2.1",     CPU_POWERPC_7448_v21,               7400)
-    /* PowerPC 7450 (G4)                                                     */
-    POWERPC_DEF("7450",          CPU_POWERPC_7450,                   7450)
     /* PowerPC 7450 v1.0 (G4)                                                */
     POWERPC_DEF("7450_v1.0",     CPU_POWERPC_7450_v10,               7450)
     /* PowerPC 7450 v1.1 (G4)                                                */
@@ -9188,6 +9185,8 @@ static const PowerPCCPUAlias ppc_cpu_aliases[] = {
     { "G4",  "7400" },
     /* Code name for PowerPC 7410 */
     { "Nitro", "7410" },
+    /* PowerPC 7450 (G4) */
+    { "7450", "7450_v2.1" },
     /* Code name for PowerPC 7450 */
     { "Vger", "7450" },
     /* PowerPC 7441 (G4) */
