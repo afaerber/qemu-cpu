dcec7319b2a28de58600a597609edacbcabe204f
Andreas Färber <afaerber@suse.de>
2013-02-18 18:13:34 +0100

    stubs: Add vmstate_register() stub
    So far we only had vmstate_register_with_alias_id().
    
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- stubs/vmstate.c
+++ stubs/vmstate.c
@@ -10,6 +10,13 @@ int vmstate_register_with_alias_id(DeviceState *dev,
     return 0;
 }
 
+int vmstate_register(DeviceState *dev, int instance_id,
+                     const VMStateDescription *vmsd, void *base)
+{
+    return vmstate_register_with_alias_id(dev, instance_id, vmsd, base,
+                                          -1, 0);
+}
+
 void vmstate_unregister(DeviceState *dev,
                         const VMStateDescription *vmsd,
                         void *opaque)
