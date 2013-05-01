601dbf1b81186dbf499109ce72c188d782137caa
Andreas Färber <afaerber@suse.de>
2013-05-01 15:10:08 +0200

    cpus: Simplify qmp_inject_nmi()
    Use new qemu_for_each_cpu().
    
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- cpus.c
+++ cpus.c
@@ -1331,18 +1331,24 @@ exit:
     fclose(f);
 }
 
+#if defined(TARGET_I386)
+static void qmp_inject_one_nmi(CPUState *cs, void *data)
+{
+    X86CPU *cpu = X86_CPU(cs);
+    CPUX86State *env = &cpu->env;
+
+    if (!env->apic_state) {
+        cpu_interrupt(cs, CPU_INTERRUPT_NMI);
+    } else {
+        apic_deliver_nmi(env->apic_state);
+    }
+}
+#endif
+
 void qmp_inject_nmi(Error **errp)
 {
 #if defined(TARGET_I386)
-    CPUArchState *env;
-
-    for (env = first_cpu; env != NULL; env = env->next_cpu) {
-        if (!env->apic_state) {
-            cpu_interrupt(CPU(x86_env_get_cpu(env)), CPU_INTERRUPT_NMI);
-        } else {
-            apic_deliver_nmi(env->apic_state);
-        }
-    }
+    qemu_for_each_cpu(qmp_inject_one_nmi, NULL);
 #else
     error_set(errp, QERR_UNSUPPORTED);
 #endif
