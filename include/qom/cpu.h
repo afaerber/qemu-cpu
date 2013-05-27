10e112041f2bb6795cc0a6e02ba2c60f9dbca691
Andreas Färber <afaerber@suse.de>
2013-05-27 03:23:24 +0200

    cpu: Change qemu_init_vcpu() argument to CPUState
    This allows to move the call to CPUState's realizefn.
    Therefore move the stub into libqemustub.a.
    
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- include/qom/cpu.h
+++ include/qom/cpu.h
@@ -407,4 +407,12 @@ void cpu_exit(CPUState *cpu);
  */
 void cpu_resume(CPUState *cpu);
 
+/**
+ * qemu_init_vcpu:
+ * @cpu: The vCPU to initialize.
+ *
+ * Initializes a vCPU.
+ */
+void qemu_init_vcpu(CPUState *cpu);
+
 #endif
