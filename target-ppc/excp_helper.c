13d926482040f57ce964a4ddead18ac1604c0b81
Andreas Färber <afaerber@suse.de>
2013-06-24 23:50:24 +0200

    cpu: Use QTAILQ for CPU list
    Introduce CPU_FOREACH(), CPU_FOREACH_SAFE() and CPU_NEXT() shorthand
    macros.
    
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- target-ppc/excp_helper.c
+++ target-ppc/excp_helper.c
@@ -992,7 +992,7 @@ void helper_msgsnd(target_ulong rb)
         return;
     }
 
-    for (cs = first_cpu; cs != NULL; cs = cs->next_cpu) {
+    CPU_FOREACH(cs) {
         PowerPCCPU *cpu = POWERPC_CPU(cs);
         CPUPPCState *cenv = &cpu->env;
 
