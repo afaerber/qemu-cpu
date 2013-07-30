74d0a94b2fd185f8c84fa3388edba6a6355bc501
Andreas Färber <afaerber@suse.de>
2013-07-30 05:02:48 +0200

    virtio-net: QOM realize preparations
    Rename variable qdev -> dev since that's what realize's argument is
    called by convention.
    
    Avoid duplicate VIRTIO_DEVICE() cast.
    
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- hw/net/virtio-net.c
+++ hw/net/virtio-net.c
@@ -1486,14 +1486,12 @@ void virtio_net_set_netclient_name(VirtIONet *n, const char *name,
 
 static int virtio_net_device_init(VirtIODevice *vdev)
 {
+    DeviceState *dev = DEVICE(vdev);
+    VirtIONet *n = VIRTIO_NET(dev);
+    NetClientState *nc;
     int i;
 
-    DeviceState *qdev = DEVICE(vdev);
-    VirtIONet *n = VIRTIO_NET(vdev);
-    NetClientState *nc;
-
-    virtio_init(VIRTIO_DEVICE(n), "virtio-net", VIRTIO_ID_NET,
-                                  n->config_size);
+    virtio_init(vdev, "virtio-net", VIRTIO_ID_NET, n->config_size);
 
     n->max_queues = MAX(n->nic_conf.queues, 1);
     n->vqs = g_malloc0(sizeof(VirtIONetQueue) * n->max_queues);
@@ -1533,7 +1531,7 @@ static int virtio_net_device_init(VirtIODevice *vdev)
                               n->netclient_type, n->netclient_name, n);
     } else {
         n->nic = qemu_new_nic(&net_virtio_info, &n->nic_conf,
-                              object_get_typename(OBJECT(qdev)), qdev->id, n);
+                              object_get_typename(OBJECT(dev)), dev->id, n);
     }
 
     peer_test_vnet_hdr(n);
@@ -1560,24 +1558,24 @@ static int virtio_net_device_init(VirtIODevice *vdev)
     nc = qemu_get_queue(n->nic);
     nc->rxfilter_notify_enabled = 1;
 
-    n->qdev = qdev;
-    register_savevm(qdev, "virtio-net", -1, VIRTIO_NET_VM_VERSION,
+    n->qdev = dev;
+    register_savevm(dev, "virtio-net", -1, VIRTIO_NET_VM_VERSION,
                     virtio_net_save, virtio_net_load, n);
 
-    add_boot_device_path(n->nic_conf.bootindex, qdev, "/ethernet-phy@0");
+    add_boot_device_path(n->nic_conf.bootindex, dev, "/ethernet-phy@0");
     return 0;
 }
 
-static int virtio_net_device_exit(DeviceState *qdev)
+static int virtio_net_device_exit(DeviceState *dev)
 {
-    VirtIONet *n = VIRTIO_NET(qdev);
-    VirtIODevice *vdev = VIRTIO_DEVICE(qdev);
+    VirtIODevice *vdev = VIRTIO_DEVICE(dev);
+    VirtIONet *n = VIRTIO_NET(dev);
     int i;
 
     /* This will stop vhost backend if appropriate. */
     virtio_net_set_status(vdev, 0);
 
-    unregister_savevm(qdev, "virtio-net", n);
+    unregister_savevm(dev, "virtio-net", n);
 
     if (n->netclient_name) {
         g_free(n->netclient_name);
