03b28eea06ae4d898f929414a08aa30410604fb4
Andreas Färber <afaerber@suse.de>
2013-09-01 17:12:23 +0200

    translate-all: Change tb_check_watchpoint() argument to CPUState
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- translate-all.h
+++ translate-all.h
@@ -29,6 +29,6 @@
 /* translate-all.c */
 void tb_invalidate_phys_page_fast(tb_page_addr_t start, int len);
 void cpu_unlink_tb(CPUState *cpu);
-void tb_check_watchpoint(CPUArchState *env);
+void tb_check_watchpoint(CPUState *cpu);
 
 #endif /* TRANSLATE_ALL_H */
