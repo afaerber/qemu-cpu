ec9c2f026cd8a00ef34c0f63049083a925248c26
Andreas Färber <afaerber@suse.de>
2015-02-06 13:18:25 +0100

    target-i386: Prepare CPU socket/core abstraction
    Short of generic recursive device realization, realize cores and threads
    recursively.
    
    Signed-off-by: Andreas Färber <afaerber@suse.de>

---
+++ include/hw/i386/cpu-core.h
@@ -0,0 +1,26 @@
+/*
+ * x86 CPU core abstraction
+ *
+ * Copyright (c) 2015 SUSE Linux GmbH
+ */
+#ifndef HW_I386_CPU_CORE_H
+#define HW_I386_CPU_CORE_H
+
+#include "hw/qdev.h"
+
+#ifdef TARGET_X86_64
+#define TYPE_X86_CPU_CORE "x86_64-cpu-core"
+#else
+#define TYPE_X86_CPU_CORE "i386-cpu-core"
+#endif
+
+#define X86_CPU_CORE(obj) \
+    OBJECT_CHECK(X86CPUCore, (obj), TYPE_X86_CPU_CORE)
+
+typedef struct X86CPUCore {
+    /*< private >*/
+    DeviceState parent_obj;
+    /*< public >*/
+} X86CPUCore;
+
+#endif
